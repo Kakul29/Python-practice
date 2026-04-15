a97f70edb1aa7d70a2452bbbbb82fb6b05490603
Kakul Jain <jainkakul29@gmail.com>
2026-04-15 20:05:01 +0530

    split_and_join.py

---
+++ problems/split_and_join.py
@@ -0,0 +1,10 @@
+# Problem: Split and Join
+# Source: HackerRank
+
+def split_and_join(line):
+    return "-".join(line.split())
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
